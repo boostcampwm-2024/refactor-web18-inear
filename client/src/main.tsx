c2419faaaab26320b5584ba6f624d3133de98274
Kang Chaeryeon <87600308+chaeryeon823@users.noreply.github.com>
2025-01-09 20:48:24 +0900

    style: Layout 컴포넌트 폴더 이동

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import './index.css';
 import { App } from '@/App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient({
   defaultOptions: {
