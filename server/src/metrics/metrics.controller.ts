db4759205a7633a32a344bebb684b238aa17c764
Yoonseo Han <yooncer00@gmail.com>
2025-01-30 01:45:26 +0900

    fix: prometheus 형식 헤더로 리턴하도록 수정

--- server/src/metrics/metrics.controller.ts
+++ server/src/metrics/metrics.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Response } from '@nestjs/common';
+import { Response as ExpressResponse } from 'express';
 import { register } from 'prom-client';
 
-@Controller('metrics') 
+@Controller('metrics')
 export class MetricsController {
   @Get()
-  async getMetrics(): Promise<string> {
-    console.log("METRICS");
-    return await register.metrics();
+  async getMetrics(@Response() res: ExpressResponse): Promise<void> {
+    console.log('METRICS');
+    res.set('Content-Type', register.contentType); 
+    res.end(await register.metrics()); 
   }
 }
